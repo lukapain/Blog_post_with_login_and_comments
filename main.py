ec0829ffa274d05c664596af4cac79192c92af13
lukapain <114741110+lukapain@users.noreply.github.com>
2022-10-25 16:52:21 +0400

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ from forms import CreatePostForm, CreateRegisterForm, LoginUser, CommentForm
 from flask_gravatar import Gravatar
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import ForeignKey
+import os
 
 app = Flask(__name__)
 login_manager = LoginManager()
 Base = declarative_base()
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app,
@@ -27,7 +28,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 login_manager.init_app(app)
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
